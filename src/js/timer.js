5e850eacac3e6c4254b76bab21bab41f7c4b3d9c
Myke003 <miguellozano81@outlook.com>
2025-05-02 17:55:28 -0500

    Completed Timer class implementation

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,65 +1,90 @@
 class Timer {
-  constructor(timeInSeconds, audio) {
+  constructor(timeInSeconds) {
+    this.initialTime = timeInSeconds;
     this.time = timeInSeconds;
     this.interval = null;
-    this.ring = new Audio(audio);
     this.isRunning = false;
-    
+    this.isPaused = false;
+
     this.timerButton = document.getElementById('timer-btn');
-    this.clock = document.getElementById('time-string'); 
+    this.clock = document.getElementById('time-string');
+
+    this.ring = new Audio('public/timer-done.mp3');
+    this.ring.addEventListener('error', () => {
+      console.log("Error al cargar el archivo de audio");
+    });
 
     this.showTime();
   }
 
   start() {
-    if(this.isRunning){
+    if (this.isRunning) {
       console.log("Esta corriendo actualmente")
       return
     }
 
     this.isRunning = true;
+    this.isPaused = false;
+    this.updateButtonText();
 
     this.interval = setInterval(() => {
-      this.showTime();
       this.time--;
+      this.showTime();
 
       if (this.time < 0) {
-        this.stop();
+        this.finish();
         this.ring.play();
       }
     }, 1000);
+
+    console.log("Is started.")
+  }
+
+  pause() {
+    if (!this.isRunning) {
+      return;
+    }
+
+    clearInterval(this.interval);
+    this.isRunning = false;
+    this.isPaused = true;
+    this.updateButtonText();
+
+    console.log("Is paused.")
+
+  }
+
+  finish(){
+    this.pause();
+    this.isPaused = false;
+    this.updateButtonText();
+    this.notificationSound();
+
+    console.log("Is finished.")
   }
 
   showTime() {
-    const minutes = Math.floor(this.time / 60); 
+    const minutes = Math.floor(this.time / 60);
     const seconds = this.time % 60;
 
     this.clock.textContent = `${minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
   }
 
-  stop() {
-    clearInterval(this.interval);
+  updateButtonText(){
+    if(this.isRunning){
+      this.timerButton.textContent = "Pause";
+    } else {
+      this.timerButton.textContent = "Start";
+    }
   }
 
-  pause(){
-    if(this.timerButton.textContent == "Start"){
-      console.log()
-    }
-  } 
-
-  notificationSound(){
-    if(this.ring){
-      this.ring.play();
-      console.log("Sonido reproducido");
-    } else{
-      console.error("No se pudo reproducir el sonido");
+  toggleStartPause() {
+    if (this.isRunning) {
+      this.pause();
+    } else {
+      this.start();
     }
   }
 }
 
-const timer = new Timer(1500, "/public/soft-bell-countdown.wav");
-
-document.getElementById('timer-btn').addEventListener("click", ()=>{
-  timer.start();
-  timer.pause();
-})
+export default Timer;
